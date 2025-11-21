68884aa38b0802c1eaee9ed462ab157c71c3e18b
mohabroha@mail.de <mohabroha@mail.de>
2025-11-21 15:04:42 +0100

    wolrdClass created

---
+++ models/world.class.js
@@ -0,0 +1,19 @@
+class World {
+
+
+
+    character = new Character();
+
+    enemies = [
+        new Chicken(),
+        new Chicken(),
+        new Chicken(),
+    ];
+
+
+
+    draw() {
+
+    }
+
+}
