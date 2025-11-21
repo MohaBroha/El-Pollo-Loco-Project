ad73fb0ee4a066920dc6ae5bdd4596d7293457f3
mohabroha@mail.de <mohabroha@mail.de>
2025-11-22 00:49:34 +0100

    newMethodesAdded

--- models/world.class.js
+++ models/world.class.js
@@ -1,7 +1,5 @@
 class World {
 
-
-
     character = new Character();
 
     enemies = [
@@ -10,10 +8,52 @@ class World {
         new Chicken(),
     ];
 
+    clouds = [
+        new Cloud()
+    ];
 
+    backgroundObjects = [
 
+        new BackgroundObjects('img/img/5_background/layers/3_third_layer/1.png', 100, 0,)
+    ];
+
+    canvas;
+    ctx;
+
+    constructor(canvas) {
+        this.ctx = canvas.getContext('2d');
+        this.canvas = canvas;
+        this.draw();
+    }
     draw() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
+        this.addobjectstoMap(this.backgroundObjects);
+        this.addobjectstoMap(this.clouds);
+        this.addobjectstoMap(this.enemies);
+        this.addtoMap(this.character);
+
+
+
+
+
+
+        let self = this;
+        requestAnimationFrame(function () {
+            self.draw();
+        });
     }
 
+    addobjectstoMap(objects) {
+        objects.forEach(o => {
+            this.addtoMap(o);
+        });
+    }
+
+    addtoMap(mo) {
+        this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
+
+    }
 }
+
+
