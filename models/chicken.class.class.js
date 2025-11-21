f900757a084560b9bc9f5ce6bec655ba0230cbac
mohabroha@mail.de <mohabroha@mail.de>
2025-11-22 00:48:32 +0100

    imgAdded

--- models/chicken.class.class.js
+++ models/chicken.class.class.js
@@ -1,7 +1,10 @@
 class Chicken extends MovableObject {
 
     constructor() {
-        super().loadImg('img/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
+        super().loadImage('img/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
+
+        this.x = 200 + Math.random() * 500;
+
     }
 
 
