671acd1ca4491416964aa6cc9dcc8152cd5841d9
mohabroha@mail.de <mohabroha@mail.de>
2025-11-22 01:35:33 +0100

    changesAdded

--- models/character.class.js
+++ models/character.class.js
@@ -1,5 +1,7 @@
 class Character extends MovableObject {
-
+    y = 155;
+    height = 280;
+    width = 150;
 
     constructor() {
         super().loadImage('img/img/2_character_pepe/1_idle/idle/I-2.png');
