b966589e3bd4288f1a20014cf3e6a73b10c9e7fe
mohabroha@mail.de <mohabroha@mail.de>
2025-11-22 01:35:54 +0100

    valueChanges

--- models/chicken.class.class.js
+++ models/chicken.class.class.js
@@ -1,5 +1,10 @@
 class Chicken extends MovableObject {
 
+    y = 330;
+    height = 100;
+    width = 90;
+
+
     constructor() {
         super().loadImage('img/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
 
