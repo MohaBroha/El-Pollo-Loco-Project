788c01f4a104da70c66a813686c2d46ed28f7795
mohabroha@mail.de <mohabroha@mail.de>
2025-11-23 18:35:36 +0100

    classCommitted

--- models/chicken.class.class.js
+++ models/chicken.class.class.js
@@ -3,14 +3,29 @@ class Chicken extends MovableObject {
     y = 330;
     height = 100;
     width = 90;
+    IMAGES_WALKING = [
+        'img/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
+        'img/img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
+        'img/img/3_enemies_chicken/chicken_normal/1_walk/3_w.png'
+    ];
 
 
     constructor() {
         super().loadImage('img/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
+        this.loadImages(this.IMAGES_WALKING);
 
         this.x = 200 + Math.random() * 500;
+        this.speed = 0.15 + Math.random() * 0.5;
+        this.animate();
 
     }
+    animate() {
+        this.moveLeft();
 
+        setInterval(() => {
+            this.playAnimation(this.IMAGES_WALKING);
+
+        }, 200);
+    }
 
 }
