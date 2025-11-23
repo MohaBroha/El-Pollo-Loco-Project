58d9b537d38e1c6c15258c7b59b2f52a339cc122
mohabroha@mail.de <mohabroha@mail.de>
2025-11-23 18:35:52 +0100

    supplementingAdded

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -1,14 +1,18 @@
 class Cloud extends MovableObject {
-
     y = 20;
-    width = 500;
     height = 250;
-
+    width = 500;
 
     constructor() {
         super().loadImage('img/img/5_background/layers/4_clouds/1.png');
 
         this.x = Math.random() * 500;
-
+        this.animate();
     }
+
+    animate() {
+        this.moveLeft();
+    }
+
+
 }
