45e2c1e7823e9d74f80104177745d56cc4b916b2
mohabroha@mail.de <mohabroha@mail.de>
2025-11-23 18:37:05 +0100

    newFunctionsCommitted

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -5,18 +5,41 @@ class MovableObject {
     img;
     height = 150;
     width = 100;
+    imageCache = {};
+    currentImage = 0;
+    speed = 0.15;
+    otherDirection = false;
+
+
 
     loadImage(path) {
         this.img = new Image();
         this.img.src = path;
     }
 
+    loadImages(arr) {
+        arr.forEach((path) => {
+            let img = new Image();
+            img.src = path;
+            this.imageCache[path] = img;
+        });
+    }
+
+    playAnimation(IMAGES_WALKING) {
+        let i = this.currentImage % this.IMAGES_WALKING.length;
+        let path = this.IMAGES_WALKING[i];
+        this.img = this.imageCache[path];
+        this.currentImage++;
+    }
 
     moveRight() {
         console.log('Moving right');
     }
 
     moveLeft() {
+        setInterval(() => {
+            this.x -= this.speed;
+        }, 1000 / 60);
 
     }
 }
