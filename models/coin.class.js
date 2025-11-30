ea8035f6115b5ce7482dda6b6b1aeeae5ec3fc05
mohabroha@mail.de <mohabroha@mail.de>
2025-12-01 00:00:22 +0100

    coinClassNewDefined

--- models/coin.class.js
+++ models/coin.class.js
@@ -1,28 +1,40 @@
+// coin.class.js
 class Coin extends MovableObject {
-    width = 50;
-    height = 50;
-    IMAGES = [
-        'img/img/8_coin/coin_1.png',
-        'img/img/8_coin/coin_2.png'
-    ];
-
-    currentImage = 0;
+    width = 120;
+    height = 120;
+    collected = false;
+    images = ['img/img/8_coin/coin_1.png', 'img/img/8_coin/coin_2.png'];
+    currentFrame = 0;
+    image = new Image();
 
     constructor(x, y) {
         super();
-        this.loadImage(this.IMAGES[0]);
-        this.loadImages(this.IMAGES);
         this.x = x;
         this.y = y;
+        this.image.src = this.images[this.currentFrame];
+        this.animate();
     }
 
     animate() {
+        setInterval(() => {
+            this.currentFrame++;
+            if (this.currentFrame >= this.images.length) this.currentFrame = 0;
+            this.image.src = this.images[this.currentFrame];
+        }, 200);
     }
 
-    playAnimation() {
-        let i = this.currentImage % this.IMAGES.length;
-        let path = this.IMAGES[i];
-        this.img = this.imageCache[path];
-        this.currentImage++;
+    draw(ctx) {
+        if (!this.collected) {
+            ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+        }
+    }
+
+    static generateCoins(startX, endX, spacing) {
+        const coins = [];
+        for (let x = startX; x <= endX; x += spacing) {
+            let y = 80 + Math.random() * 20;
+            coins.push(new Coin(x, y));
+        }
+        return coins;
     }
 }
