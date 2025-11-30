e07cc80cfce73e9dccabe2ce70cd3eaf61645140
mohabroha@mail.de <mohabroha@mail.de>
2025-12-01 00:02:31 +0100

    objectsNewDefined

--- models/throwable-object.js
+++ models/throwable-object.js
@@ -1,20 +1,29 @@
 class ThrowableObject extends MovableObject {
-
+    speedY = 15;
+    acceleration = 1;
+    direction = 1;
 
     constructor(x, y) {
-        super().loadImage('img/img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png');
+        super().loadImage('img/img/6_salsa_bottle/1_salsa_bottle_on_ground.png');
+
         this.x = x;
         this.y = y;
-        this.height = 60;
+
         this.width = 40;
+        this.height = 50;
+
+        this.offset = { top: 10, bottom: 10, left: 10, right: 10 };
+
         this.throw();
     }
 
+
     throw() {
-        this.speedY = 30;
+        this.speedY = 14;
         this.applyGravity();
+
         setInterval(() => {
-            this.x += 10;
+            this.x += 10 * this.direction;
         }, 25);
     }
 }
