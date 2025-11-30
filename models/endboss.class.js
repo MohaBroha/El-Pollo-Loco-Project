7867c8d08e0262626b6e21c8b3e620cba0ce9518
mohabroha@mail.de <mohabroha@mail.de>
2025-12-01 00:01:19 +0100

    dataChanges

--- models/endboss.class.js
+++ models/endboss.class.js
@@ -22,7 +22,7 @@ class Endboss extends MovableObject {
     constructor() {
         super().loadImage(this.IMAGES_WALKING[0]);
         this.loadImages(this.IMAGES_WALKING);
-        this.x = 2700;
+        this.x = 3000;
         this.animate();
 
     }
