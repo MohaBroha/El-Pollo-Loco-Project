fb83c42b0b69c85750d0ed5211614fb9baf47995
mohabroha@mail.de <mohabroha@mail.de>
2025-12-09 23:38:13 +0100

    moreClassFunctionSupplemented

--- js/game.js
+++ js/game.js
@@ -2,52 +2,105 @@ let canvas;
 let ctx;
 let world;
 let keyboard = new Keyboard();
-let bgMusic = new Sound('audio/juego-peligroso-background-latin-vlog-music-for-video-stories-379503.mp3');
 let gameStarted = false;
+let gameEnded = false;
+let allIntervals = [];
+
+let bgMusic;
 
 let startScreenImage = new Image();
 startScreenImage.src = 'img/img/9_intro_outro_screens/start/startscreen_1.png';
 
+let startScreenAnimationId;
+
+function setStoppableInterval(fn, time) {
+    let id = setInterval(fn, time);
+    allIntervals.push(id);
+    return id;
+}
+
 function drawStartScreen() {
-    if (!ctx) return;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.drawImage(startScreenImage, 0, 0, canvas.width, canvas.height);
-    requestAnimationFrame(drawStartScreen);
+
+    startScreenAnimationId = requestAnimationFrame(drawStartScreen);
+
 }
 
 function init() {
+    Sound.init();
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
 
-    // Startscreen nicht mehr animieren
-    cancelAnimationFrame(drawStartScreen);
+    cancelAnimationFrame(startScreenAnimationId);
 
-    // Welt starten
+    initLevel();
+    world = new World(canvas, keyboard);
+
+    gameStarted = true;
+    gameEnded = false;
+
+    bgMusic = Sound.playSound('audio/juego-peligroso-background-latin-vlog-music-for-video-stories-379503.mp3');
+
+
+}
+
+function restartGame() {
+    gameEnded = false;
+
+    keyboard.RIGHT = false;
+    keyboard.LEFT = false;
+    keyboard.UP = false;
+    keyboard.DOWN = false;
+    keyboard.SPACE = false;
+    keyboard.D = false;
+
+    Sound.muteAll(false);
+
+    allIntervals.forEach(id => clearInterval(id));
+    allIntervals = [];
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    initLevel();
     world = new World(canvas, keyboard);
 
     gameStarted = true;
 
-    // Musik starten
     bgMusic.play();
 }
 
-// Button Event
-const startBtn = document.getElementById('startButton');
-startBtn.addEventListener('click', () => {
-    startBtn.style.display = 'none';
-    init();
-});
 
-// Startscreen anzeigen, bis auf Start gedrückt
+function showEndScreen(won = false) {
+    gameEnded = true;
+    Sound.muteAll(true);
+
+
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.globalAlpha = 0.75;
+
+    const img = new Image();
+    img.src = won
+        ? 'img/img/You won, you lost/You Won A.png'
+        : 'img/img/You won, you lost/Game Over.png';
+
+    img.onload = () => {
+        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+        ctx.globalAlpha = 1;
+        document.getElementById('playAgainBtn').style.display = 'block';
+    };
+}
+
 window.onload = () => {
+
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
     drawStartScreen();
-}
+};
 
-// Keyboard-Events nur, wenn Spiel gestartet
 window.addEventListener("keydown", (e) => {
-    if (!gameStarted) return;
+    if (!gameStarted || gameEnded || buttonActive) return;
     switch (e.keyCode) {
         case 39: keyboard.RIGHT = true; break;
         case 37: keyboard.LEFT = true; break;
@@ -59,7 +112,7 @@ window.addEventListener("keydown", (e) => {
 });
 
 window.addEventListener("keyup", (e) => {
-    if (!gameStarted) return;
+    if (!gameStarted || gameEnded || buttonActive) return;
     switch (e.keyCode) {
         case 39: keyboard.RIGHT = false; break;
         case 37: keyboard.LEFT = false; break;
