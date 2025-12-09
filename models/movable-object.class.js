0abac54675da8634ae9cdccc8370a997f25a97b8
mohabroha@mail.de <mohabroha@mail.de>
2025-12-09 23:45:52 +0100

    isDeadFunctionAndMoreCreated

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,4 +1,3 @@
-
 class MovableObject extends DrawableObject {
 
     speed = 0.15;
@@ -7,6 +6,11 @@ class MovableObject extends DrawableObject {
     acceleration = 2.5;
     energy = 100;
     lastHit = 0;
+    currentImage = 0;
+
+    deathImage = null;
+    deathSound = null;
+    isKilled = false;
 
     applyGravity() {
         setInterval(() => {
@@ -14,10 +18,9 @@ class MovableObject extends DrawableObject {
                 this.y -= this.speedY;
                 this.speedY -= this.acceleration;
             }
-
         }, 1000 / 25);
-
     }
+
     isAboveGround() {
         if (this instanceof ThrowableObject) {
             return true;
@@ -29,9 +32,10 @@ class MovableObject extends DrawableObject {
     isColliding(mo) {
         return this.x + this.width > mo.x &&
             this.y + this.height > mo.y &&
-            this.x < mo.x &&
-            this.y < mo.y + mo.height
+            this.x < mo.x + mo.width &&
+            this.y < mo.y + mo.height;
     }
+
     hit() {
         this.energy -= 5;
         if (this.energy < 0) {
@@ -40,6 +44,7 @@ class MovableObject extends DrawableObject {
             this.lastHit = new Date().getTime();
         }
     }
+
     isHurt() {
         let timepassed = new Date().getTime() - this.lastHit;
         timepassed = timepassed / 1000;
@@ -47,10 +52,8 @@ class MovableObject extends DrawableObject {
     }
 
     isDead() {
-        return this.energy == 0;
+        return this.energy == 0 || this.isKilled;
     }
-
-
 
     playAnimation(images) {
         let i = this.currentImage % images.length;
@@ -71,5 +74,35 @@ class MovableObject extends DrawableObject {
 
     jump() {
         this.speedY = 30;
+    }
+
+    isAttacking() {
+        if (!this.world || !this.world.character) return false;
+        return this.x - this.world.character.x < 400 && this.energy > 0;
+    }
+
+    attackHitbox() {
+        return {
+            x: this.x - 50,
+            y: this.y,
+            width: this.width + 100,
+            height: this.height
+        };
+    }
+
+    playDeath() {
+        if (this.isKilled) return;
+
+        this.isKilled = true;
+
+        if (this.deathImage) {
+            this.loadImage(this.deathImage);
+        }
+
+        if (this.deathSound) {
+            const s = new Audio(this.deathSound);
+            s.volume = 0.3;
+            s.play();
+        }
     }
 }
